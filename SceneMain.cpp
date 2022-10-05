9d0827b8a5aa245b46ac0936dfffbe42aba9195c
ユリカ007 <yudai1023.ta@gmail.com>
2022-10-05 12:09:14 +0900

    追加

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -46,6 +46,25 @@ void SceneMain::end()
 {
 	DeleteGraph(m_hPlayerGraphic);
 	DeleteGraph(m_hShotGraphic);
+
+	for (auto& shot : m_pShotNormal)
+	{
+		if (!shot) continue;
+		delete shot;
+		shot = nullptr;
+	}
+	for (auto& shot : m_pShotSin)
+	{
+		if (!shot) continue;
+		delete shot;
+		shot = nullptr;
+	}
+	for (auto& shot : m_pShotBound)
+	{
+		if (!shot) continue;
+		delete shot;
+		shot = nullptr;
+	}
 }
 
 // 毎フレームの処理
@@ -56,16 +75,31 @@ void SceneMain::update()
 	{
 		if (!shot) continue;
 		shot->update();
+		if (!shot->isExist())
+		{
+			delete shot;
+			shot = nullptr;
+		}
 	}
 	for (auto& shot : m_pShotSin)
 	{
 		if (!shot) continue;
 		shot->update();
+		if (!shot->isExist())
+		{
+			delete shot;
+			shot = nullptr;
+		}
 	}
 	for (auto& shot : m_pShotBound)
 	{
 		if (!shot) continue;
 		shot->update();
+		if (!shot->isExist())
+		{
+			delete shot;
+			shot = nullptr;
+		}
 	}
 }
 
@@ -74,72 +108,81 @@ void SceneMain::draw()
 {
 	m_player.draw();
 
-	for (auto& shot : m_shotNormal)
+	for (auto& shot : m_pShotNormal)
 	{
-		shot.draw();
+		if (!shot) continue;
+		shot->draw();
 	}
-	for (auto& shot : m_shotSin)
+	for (auto& shot : m_pShotSin)
 	{
-		shot.draw();
+		if (!shot) continue;
+		shot->draw();
 	}
-	for (auto& shot : m_shotBound)
+	for (auto& shot : m_pShotBound)
 	{
-		shot.draw();
+		if (!shot) continue;
+		shot->draw();
 	}
 
 	// 現在存在している玉の数を表示
 	int shotNum = 0;
-	for (auto& shot : m_shotNormal)
+	for (auto& shot : m_pShotNormal)
 	{
-		if (shot.isExist()) shotNum++;
+		if (!shot) continue;
+		if (shot->isExist()) shotNum++;
 	}
 	DrawFormatString(0, 0, GetColor(225, 225, 225), "normal：%d", shotNum);
 	shotNum = 0;
-	for (auto& shot : m_shotSin)
+	for (auto& shot : m_pShotSin)
 	{
-		if (shot.isExist()) shotNum++;
+		if (!shot) continue;
+		if (shot->isExist()) shotNum++;
 	}
 	DrawFormatString(0, 30, GetColor(225, 225, 225), "sin：%d", shotNum);
 	shotNum = 0;
-	for (auto& shot : m_shotBound)
+	for (auto& shot : m_pShotBound)
 	{
-		if (shot.isExist()) shotNum++;
+		if (!shot) continue;
+		if (shot->isExist()) shotNum++;
 	}
 	DrawFormatString(0, 60, GetColor(225, 225, 225), "bound：%d", shotNum);
 }
 
 bool SceneMain::createShotNormal(Vec2 pos)
 {
-	for (auto& shot : m_shotNormal)
+	for (auto& shot : m_pShotNormal)
 	{
-		if (shot.isExist()) continue;
-		
-		shot.start(pos);
+		if (shot) continue;
+
+		shot = new ShotNormal;
+		shot->setHandle(m_hShotGraphic);
+		shot->start(pos);
 		return true;
 	}
-	return false;
 }
 
 bool SceneMain::createShotSin(Vec2 pos)
 {
-	for (auto& shot : m_shotSin)
+	for (auto& shot : m_pShotSin)
 	{
-		if (shot.isExist()) continue;
+		if (shot) continue;
 
-		shot.start(pos);
+		shot = new ShotSin;
+		shot->setHandle(m_hShotGraphic);
+		shot->start(pos);
 		return true;
 	}
-	return false;
 }
 
 bool SceneMain::createShotBound(Vec2 pos)
 {
-	for (auto& shot : m_shotBound)
+	for (auto& shot : m_pShotBound)
 	{
-		if (shot.isExist()) continue;
+		if (shot) continue;
 
-		shot.start(pos);
+		shot = new ShotBound;
+		shot->setHandle(m_hShotGraphic);
+		shot->start(pos);
 		return true;
 	}
-	return false;
 }
